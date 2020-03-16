0b3bbe576e80c2a7b0155b5fa161cf3687502474
SIJU JACOB <SIJUJACOBS@GMAILCOM>
2020-03-16 19:26:51 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { DocumentComponent } from './document/document.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ProductComponent,
+    DocumentComponent
   ],
   imports: [
     BrowserModule,
